4fbc0578214e55dc4ce0c68db022688c430f8c01
ArachDyrr <1021548@hr.nl>
2023-04-05 16:35:15 +0200

    end of day matrix class commit

--- practica/07_ml_frameworks/HombrewMatrix.py
+++ practica/07_ml_frameworks/HombrewMatrix.py
@@ -1,12 +1,14 @@
 class Matrix:
-    # create the check to see if the matrix has the same amount of items per row
+    """Homebrew Matrix class"""
     def __init__(self, argInput):
         self.input = argInput
 
-    def getListOfLists (self): # generates a printable object
+    def printable (self): # generates a printable object
+        """generates a printable object"""
         return (self.input)
 
     def transpose (self):
+        """transposes a matrix"""
         transposed_input = list(map(list, zip(*self.input)))
         return transposed_input
 
@@ -22,23 +24,23 @@ class Matrix:
             row_nr += 1
             return True
 
-    def matrix_multiplication(self, other_matrix):
+    def matrix_multiplication(self, other):
         """multiplies two matrices if permitted"""
         # check if both matrices have the proper shape to be multiplied
 
-        if len(self.input[0]) == len(other_matrix.input):
+        if len(self.input[0]) == len(other.input):
             pass
             # DeMorgan
             # not (A and B) == not A or not B
 
-            if not (self.matrix_shape_check() and other_matrix.matrix_shape_check()):
+            if not (self.matrix_shape_check() and other.matrix_shape_check()):
                 return None
         else:
             print('Matrix shapes do not match!')
             return None
 
         # transpose the second matrix
-        other_matrix_t = list(map(list, zip(*other_matrix.input)))
+        other_t = list(map(list, zip(*other.input)))
 
         # create the new matrix for the multiplication result
         result_matrix = []
@@ -46,8 +48,8 @@ class Matrix:
         # cycle through the matrices to calculate the cells for result_matrix row by row and add them to the new matrix
         for i in range(len(self.input)):
             row = []
-            for j in range(len(other_matrix.input[0])):
-                cell = list(map(lambda x, y: x * y, self.input[i], other_matrix_t[j]))
+            for j in range(len(other.input[0])):
+                cell = list(map(lambda x, y: x * y, self.input[i], other_t[j]))
                 cell_sum = sum(cell)
                 row.append(cell_sum)
             result_matrix.append(row)
@@ -60,7 +62,9 @@ class Matrix:
         return all(len(row) == shape for row in self.input)
 
     def __eq__ (self, other):
+
         return len(self.input[0]) == len(other.input)
 
     def __mul__(self, other):
+        """multiplies 2 matrixes"""
         return self.matrix_multiplication(other)
